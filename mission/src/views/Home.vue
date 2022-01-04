7302fcebb6d383a808c7098d8651b61a10e61042
jambottle <jaewon.walks@gmail.com>
2022-01-05 06:10:29 +0900

    Add: 1주차 미션 요구사항 구현 - v-model 디렉티브를 활용하여 input 태그 입력값의 양방향 데이터 바인딩 구현 - 버튼별 이벤트 발생에 따라 호출되어야 하는 문자열 회전 및 알림창 함수 구현

--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -2,17 +2,42 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <!-- 1주차 미션: 간단한 문자열 회전 기능 구현해보기 -->
+    <input type="text" v-model="inputValue" />
+    <button @click="handleInput">제출</button>
+    <button @click="alertInput">알림</button>
+    <p>{{ inputValue }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
+
   components: {
     HelloWorld,
+  },
+
+  data() {
+    return {
+      inputValue: "",
+      click: 0,
+    };
+  },
+
+  methods: {
+    handleInput() {
+      const str1 = this.inputValue.slice(1);
+      const str2 = this.inputValue.slice(0, 1);
+      this.inputValue = str1 + str2;
+    },
+    alertInput() {
+      this.click += 1;
+      alert(`${this.inputValue} ${this.click}`);
+    },
   },
 };
 </script>
