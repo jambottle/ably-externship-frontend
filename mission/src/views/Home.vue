e5cfa82f7fcd66aecc11cb0d98e107a2099b6ee6
jambottle <jaewon.walks@gmail.com>
2022-01-09 05:30:55 +0900

    Modify: ESLint의 no-alert 경고에 대한 조치로 Modal 컴포넌트 분리 - alert 함수로 알림창 구현 시, ESLint가 Custom UI의 구현으로 알림창을 대체할 것을 권고 - Modal.vue 파일로 알림창 컴포넌트를 분리하고, v-if 디렉티브와 slot으로 세부 기능 구현

--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -3,40 +3,54 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
 
-    <!-- 1주차 미션: 간단한 문자열 회전 기능 구현해보기 -->
-    <input type="text" v-model="inputValue" />
-    <button @click="handleInput">제출</button>
-    <button @click="alertInput">알림</button>
-    <p>{{ inputValue }}</p>
+    <!-- 1주차 미션 (기본): 간단한 문자열 회전 기능 구현해보기 -->
+    <input type="text" v-model="inputText" />
+    <button @click="rotateText">제출</button>
+    <button @click="showModal">알림</button>
+    <p>{{ inputText }}</p>
+
+    <!-- 1주차 미션 (심화): 입력된 문자열과 버튼 클릭 횟수를 알림창에 띄우기 -->
+    <Modal v-if="isModalShown" @close="isModalShown = false">
+      <template v-slot:header>
+        <h3>Result</h3>
+      </template>
+      <template v-slot:body>
+        <p><b>Text:</b> {{ inputText }}</p>
+        <p><b>Clicks:</b> {{ clicks }}</p>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
   name: "Home",
 
   components: {
     HelloWorld,
+    Modal,
   },
 
   data() {
     return {
-      inputValue: "",
-      click: 0,
+      inputText: "",
+      isModalShown: false,
+      clicks: 0,
     };
   },
 
   methods: {
-    handleInput() {
-      const str1 = this.inputValue.slice(1);
-      const str2 = this.inputValue.slice(0, 1);
-      this.inputValue = str1 + str2;
+    rotateText() {
+      const str1 = this.inputText.slice(1);
+      const str2 = this.inputText.slice(0, 1);
+      this.inputText = str1 + str2;
     },
-    alertInput() {
-      this.click += 1;
-      alert(`${this.inputValue} ${this.click}`);
+    showModal() {
+      this.isModalShown = true;
+      this.clicks += 1;
     },
   },
 };
