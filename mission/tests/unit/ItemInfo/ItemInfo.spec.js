8f1e530a5421fce8163beb5c7d1c930da259f1d4
jambottle <jaewon.walks@gmail.com>
2022-01-16 21:10:17 +0900

    Add: 2주차 미션 요구사항 구현 - 앞서 작성한 테스트 코드를 바탕으로 ItemInfo 컴포넌트의 전반적인 레이아웃 구현 및 스타일링 - 스타일링 시, CSS 스타일링 규칙만을 가지고 있는 W3.CSS 프레임워크를 main.js에 일괄 적용

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -1,10 +1,40 @@
 import { mount } from '@vue/test-utils';
-import ItemInfoPage from '@/views/ItemInfo.vue';
+import ItemInfo from '@/views/ItemInfo.vue';
 
-describe('ItemInfoPage', () => {
-  it('redners ItemInfoPage', () => {
-    const wrapper = mount(ItemInfoPage);
+describe('ItemInfo.vue', () => {
+  const wrapper = mount(ItemInfo);
 
-    expect(wrapper.find('#item-info-page').exists()).toBe(true);
+  it('renders profile image of item', () => {
+    expect(wrapper.get('div[data-test="item-profile"]').exists()).toBe(true);
+  });
+
+  it('renders profile image, name and hashtags of shop', () => {
+    expect(wrapper.get('div[data-test="shop-profile"]').exists()).toBe(true);
+    expect(wrapper.get('b[data-test="shop-name"]').exists()).toBe(true);
+    expect(wrapper.get('span[data-test="shop-tags"]').exists()).toBe(true);
+  });
+
+  it('renders discount rate, discount price and original price of item', () => {
+    expect(wrapper.get('h2[data-test="item-name"]').exists()).toBe(true);
+    expect(wrapper.get('b[data-test="discount-rate"]').exists()).toBe(true);
+    expect(wrapper.get('span[data-test="discount-price"]').exists()).toBe(true);
+    expect(wrapper.get('del[data-test="original-price"]').exists()).toBe(true);
+  });
+
+  it('has paragraph element for description of item', () => {
+    expect(wrapper.get('p[data-test="item-desc"]').exists()).toBe(true);
+  });
+
+  it('displays reviews with post and attatched photo of each review', () => {
+    expect(wrapper.get('b[data-test="review-name"]').exists()).toBe(true);
+    expect(wrapper.get('span[data-test="review-date"]').exists()).toBe(true);
+    expect(wrapper.get('h6[data-test="review-title"]').exists()).toBe(true);
+    expect(wrapper.get('p[data-test="review-content"]').exists()).toBe(true);
+    expect(wrapper.get('div[data-test="review-photo"]').exists()).toBe(true);
+  });
+
+  it('shows fixed footer with a button which contains discount price', () => {
+    expect(wrapper.get('footer[data-test="footer-wrapper"]').exists()).toBe(true);
+    expect(wrapper.get('button[data-test="footer-button"]').exists()).toBe(true);
   });
 });
