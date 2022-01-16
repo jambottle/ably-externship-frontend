25da41ffcdc6c33588c892f4ae5716940e523d3f
jambottle <jaewon.walks@gmail.com>
2022-01-16 18:30:30 +0900

    Add: 상품정보 및 리뷰 Mock Data 작성 및 추가

---
+++ mission/src/assets/itemData.js
@@ -0,0 +1,16 @@
+export default {
+  shopName: '브라운슈즈',
+  shopTags: ['운동화', '스트릿웨어'],
+  shopProfile: 'https://images.unsplash.com/photo-1622408298915-24d322badf9e',
+  itemName: '뉴욕 스트릿 풋웨어 1',
+  itemDesc: `
+    <span style="font-weight: bold">도시의 감성을 당신의 걸음에 더하세요! 🥾</span>
+    <img src="https://images.unsplash.com/photo-1519429270060-72b015505fdf" style="width: 100%; margin: 12px 0 16px" />
+    촉촉한 가죽 터치와 주름에 강한 가죽으로 신을수록 자연스러움을 만들어 가죽 특유의 내츄럴한 느낌을 연출해주며, 군용화와 워커에 쓰이는 강력한 가죽으로 기스에도 강해 도심 속 어디서나 자연스러운 멋을 유지합니다.
+  `,
+  itemProfile: 'https://images.unsplash.com/photo-1549298916-b41d501d3772',
+  itemPrice: {
+    original: 298000,
+    discount: 198000,
+  },
+};
---
+++ mission/src/assets/reviewData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 7,
+    userName: 'H.M. Son',
+    postDate: '2022. 01. 16',
+    postTitle: '매우 만족스러워요!!',
+    postContent: '뉴욕뿐만 아니라 런던에서도 착화감이 가볍고 생활 방수도 거뜬해서 편하게 신고 다니기에 무척 좋아요 🤠',
+    postPhoto: 'https://images.unsplash.com/photo-1578608712688-36b5be8823dc',
+  },
+  {
+    id: 11,
+    userName: 'Bryan Gil',
+    postDate: '2022. 01. 15',
+    postTitle: '만족스러워요!',
+    postContent: '아는 선배가 신길래 따라 신어봤는데 완전 대만족 🤙 이건 저한테 어울리는 거 같아요~',
+    postPhoto: 'https://images.unsplash.com/photo-1595459802544-d6cad89f7ae8',
+  },
+  {
+    id: 15,
+    userName: 'Eric Dier',
+    postDate: '2022. 01. 14',
+    postTitle: '그저 그래요.',
+    postContent: '저한테는 잘 어울리지 않아서 살짝 아쉬워요... 혹시 반품 될까요? 😅',
+    postPhoto: '',
+  },
+];
