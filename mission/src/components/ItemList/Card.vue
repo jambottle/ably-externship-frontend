3a64fa9b99b81063b582a5f456d6e86cff6e4d6e
jambottle <jaewon.walks@gmail.com>
2022-01-23 21:30:29 +0900

    Add: 3주차 미션 요구사항 구현 - (1) Card 컴포넌트 (Card.vue) - Header 영역과 Nav 영역 사이에 2열로 표시될 상품 정보 카드의 레이아웃 구현 및 스타일링 - 상품 정보 카드는 상품의 사진과 이름, 가격을 포함하고, 할인율을 조건부 렌더링으로 표시

---
+++ mission/src/components/ItemList/Card.vue
@@ -0,0 +1,94 @@
+<template>
+  <article id="card">
+    <div
+      class="item-profile"
+      data-test="item-profile"
+      :style="`background-image: url(${item.profile})`"
+    />
+
+    <p v-if="isDiscounted" class="item-price">
+      <b data-test="discount-rate">{{ discountRate }}%&nbsp;</b>
+      <span data-test="discount-price">{{ discountPrice }}원</span>
+    </p>
+    <p v-else class="item-price">
+      <span data-test="discount-price">{{ discountPrice }}원</span>
+    </p>
+
+    <h2 class="item-name" data-test="item-name">{{ item.name }}</h2>
+    <p class="item-desc">상품 설명이 길어질 경우에는 다음과 같이 표시됩니다.</p>
+  </article>
+</template>
+
+<script>
+import itemData from '@/assets/itemData';
+
+export default {
+  name: 'ItemListCard',
+
+  data() {
+    return {
+      ...itemData,
+    };
+  },
+
+  computed: {
+    isDiscounted() {
+      return this.item.price.discount !== this.item.price.original;
+    },
+    discountRate() {
+      const rate = 1 - this.item.price.discount / this.item.price.original;
+      return Math.round(rate * 100);
+    },
+    discountPrice() {
+      return this.item.price.discount.toLocaleString();
+    },
+    originalPrice() {
+      return this.item.price.original.toLocaleString();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#card {
+  width: 50%;
+
+  .item-profile {
+    width: 160px;
+    height: 160px;
+    margin: 12px 0 8px;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .item-price {
+    margin-top: 16px;
+
+    b {
+      color: red;
+      font-size: 16px;
+      font-weight: bold;
+    }
+
+    span {
+      color: black;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+
+  .item-name {
+    margin: 6px 0;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .item-desc {
+    width: 180px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 16px;
+  }
+}
+</style>
