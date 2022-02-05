87c190680633b59796128aa91844e61fb8a22c9a
jambottle <jaewon.walks@gmail.com>
2022-02-06 01:00:13 +0900

    Refactor: 테스트 코드 리팩토링 - (4) ItemList 페이지에 대한 기존 테스트 코드 수정 - 리스트 렌더링에 활용될 임의의 데이터(Array 타입)를 setData 메서드로 테스트에 주입 - Card 컴포넌트가 리스트 렌더링으로 반복되는 횟수를 findAllComponents 메서드로 검증 - for 반복문을 활용해 컴포넌트별로 포함해야 할 요소들이 제대로 렌더링 되고 있는지 확인

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,14 +1,31 @@
 import { mount } from '@vue/test-utils';
 import ItemList from '@/views/ItemList.vue';
+import ItemListCard from '@/components/ItemList/Card.vue';
+
+const testItemList = [
+  { name: 'name 1', desc: 'description 1', price: { discount: 13400 } },
+  { name: 'name 2', desc: 'description 2', price: { discount: 24500 } },
+  { name: 'name 3', desc: 'description 3', price: { discount: 35600 } },
+];
 
 describe('ItemList.vue', () => {
-  const wrapper = mount(ItemList);
+  it('renders multiple ItemListCard components', async () => {
+    const wrapper = mount(ItemList);
 
-  it('redners header element', () => {
-    expect(wrapper.get('header[data-test="header-wrapper"]').exists()).toBe(true);
-  });
+    await wrapper.setData({
+      items: testItemList,
+    });
 
-  it('redners nav element', () => {
-    expect(wrapper.get('nav[data-test="footer-wrapper"]').exists()).toBe(true);
+    const listWrapper = wrapper.findAllComponents(ItemListCard);
+
+    expect(listWrapper).toHaveLength(testItemList.length);
+
+    for (let i = 0; i < testItemList.length; i += 1) {
+      const cardWrapper = listWrapper[i];
+
+      expect(cardWrapper.get('span[data-test="discount-price"]').text()).toBe(`${testItemList[i].price.discount.toLocaleString()}원`);
+      expect(cardWrapper.get('h2[data-test="item-name"]').text()).toBe(testItemList[i].name);
+      expect(cardWrapper.get('p[data-test="item-desc"]').text()).toBe(testItemList[i].desc);
+    }
   });
 });
