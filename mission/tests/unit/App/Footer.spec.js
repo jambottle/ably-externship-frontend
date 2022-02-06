7a5c5a52c15f392a2e9644401c2942bf7f324c40
jambottle <jaewon.walks@gmail.com>
2022-02-06 16:55:18 +0900

    Modify: Header/Footer 컴포넌트에 대한 테스트 코드 내에서 import 경로 수정

--- mission/tests/unit/App/Footer.spec.js
+++ mission/tests/unit/App/Footer.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils';
-import ItemListFooter from '@/components/ItemList/Footer.vue';
+import AppFooter from '@/components/App/Footer.vue';
 
 describe('Footer.vue', () => {
   it('renders nav element with links', () => {
-    const wrapper = mount(ItemListFooter);
+    const wrapper = mount(AppFooter);
 
     expect(wrapper.get('nav[data-test="footer-wrapper"]').exists()).toBe(true);
     expect(wrapper.get('nav[data-test="footer-wrapper"] router-link').exists()).toBe(true);
--- mission/tests/unit/App/Header.spec.js
+++ mission/tests/unit/App/Header.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils';
-import ItemListHeader from '@/components/ItemList/Header.vue';
+import AppHeader from '@/components/App/Header.vue';
 
 describe('Header.vue', () => {
   it('renders header element with title', () => {
-    const wrapper = mount(ItemListHeader);
+    const wrapper = mount(AppHeader);
 
     expect(wrapper.get('header[data-test="header-wrapper"]').exists()).toBe(true);
     expect(wrapper.get('header[data-test="header-wrapper"] h1').exists()).toBe(true);
