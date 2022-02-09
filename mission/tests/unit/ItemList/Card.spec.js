229df7d820d2dd7360c825c42535755dfc1904cf
jambottle <jaewon.walks@gmail.com>
2022-02-10 06:10:41 +0900

    Test: Card 컴포넌트에 대한 테스트 코드 정리 - 테스트 내용의 변동은 없으며, 다른 테스트 케이스들과의 테스트 작성 컨벤션 통일

--- mission/tests/unit/ItemList/Card.spec.js
+++ mission/tests/unit/ItemList/Card.spec.js
@@ -1,27 +1,25 @@
 import { mount } from '@vue/test-utils';
 import ItemListCard from '@/components/ItemList/Card.vue';
 
-const testItemName = '아이언맨 마크 42';
-const testItemDesc = '원거리 탈착 기능이 탑재된 고성능 슈트';
-const testItemOriginalPrice = 5000000000;
-const testItemDiscountPrice = 4000000000;
-const testItemDiscountRate = 1 - (testItemDiscountPrice / testItemOriginalPrice);
+const testItemInfo = {
+  name: '아이언맨 마크 42',
+  desc: '<p>원거리 탈착 기능이 탑재된 고성능 슈트</p>',
+  discount_price: 4000000000,
+  original_price: 5000000000,
+};
 
 describe('Card.vue', () => {
   it('renders profile image, name, discount rate and price of item', async () => {
     const wrapper = mount(ItemListCard);
 
     await wrapper.setProps({
-      name: testItemName,
-      desc: testItemDesc,
-      discount_price: testItemDiscountPrice,
-      original_price: testItemOriginalPrice,
+      ...testItemInfo,
     });
 
     expect(wrapper.get('div[data-test="item-profile"]').exists()).toBe(true);
-    expect(wrapper.get('b[data-test="discount-rate"]').text()).toBe(`${Math.round(testItemDiscountRate * 100)}%`);
-    expect(wrapper.get('span[data-test="discount-price"]').text()).toBe(`${testItemDiscountPrice.toLocaleString()}원`);
-    expect(wrapper.get('h2[data-test="item-name"]').text()).toBe(testItemName);
-    expect(wrapper.get('p[data-test="item-desc"]').text()).toBe(testItemDesc);
+    expect(wrapper.get('b[data-test="discount-rate"]').text()).toBe(`${Math.round((1 - (testItemInfo.discount_price / testItemInfo.original_price)) * 100)}%`);
+    expect(wrapper.get('span[data-test="discount-price"]').text()).toBe(`${testItemInfo.discount_price.toLocaleString()}원`);
+    expect(wrapper.get('h2[data-test="item-name"]').text()).toBe(testItemInfo.name);
+    expect(wrapper.get('p[data-test="item-desc"]').text()).toBe(testItemInfo.desc);
   });
 });
