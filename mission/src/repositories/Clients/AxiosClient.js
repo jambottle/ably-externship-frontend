350a9c65ab3b72027a62dcddc55df95989399c25
jambottle <jaewon.walks@gmail.com>
2022-02-13 05:10:52 +0900

    Refactor: API 통신에 필요한 baseURL 및 Authorization Key를 .env로 이동 - 앞으로 API 통신에 있어 민감한 정보들을 .env에서 환경 변수로 관리하기 위해 설정

--- mission/src/repositories/Clients/AxiosClient.js
+++ mission/src/repositories/Clients/AxiosClient.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-const baseDomain = 'https://virtserver.swaggerhub.com/lkaybob/projectlion-vue/1.0.0';
-const baseURL = `${baseDomain}`;
-
 export default axios.create({
-  baseURL,
+  baseURL: process.env.VUE_APP_BASE_URL,
   headers: {
-    Authorization: 'abcd1234',
+    Authorization: process.env.VUE_APP_AUTH_KEY,
   },
 });
