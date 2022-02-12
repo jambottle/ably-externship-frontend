98155fe0a5c52c0aa1650a6dd315a80c41f50489
jambottle <jaewon.walks@gmail.com>
2022-02-13 01:45:10 +0900

    Modify: Card 컴포넌트 클릭 시, 페이지를 전환하는 방식 변경 - Vue Router의 내장 메서드를 이벤트로 활용하는 대신, HTML 태그(router-link)를 채택

--- mission/src/components/ItemList/Card.vue
+++ mission/src/components/ItemList/Card.vue
@@ -1,21 +1,23 @@
 <template>
   <article id="card">
-    <div
-      class="item-profile"
-      data-test="item-profile"
-      :style="`background-image: url(${profile})`"
-    />
+    <router-link :to="`/item/${id}`">
+      <div
+        class="item-profile"
+        data-test="item-profile"
+        :style="`background-image: url(${profile})`"
+      />
 
-    <p v-if="isDiscounted" class="item-price">
-      <b data-test="discount-rate">{{ discountRate }}%&nbsp;</b>
-      <span data-test="discount-price">{{ discountPrice }}원</span>
-    </p>
-    <p v-else class="item-price">
-      <span data-test="discount-price">{{ discountPrice }}원</span>
-    </p>
+      <p v-if="isDiscounted" class="item-price">
+        <b data-test="discount-rate">{{ discountRate }}%&nbsp;</b>
+        <span data-test="discount-price">{{ discountPrice }}원</span>
+      </p>
+      <p v-else class="item-price">
+        <span data-test="discount-price">{{ discountPrice }}원</span>
+      </p>
 
-    <h2 class="item-name" data-test="item-name">{{ name }}</h2>
-    <p class="item-desc" data-test="item-desc">{{ desc }}</p>
+      <h2 class="item-name" data-test="item-name">{{ name }}</h2>
+      <p class="item-desc" data-test="item-desc">{{ desc }}</p>
+    </router-link>
   </article>
 </template>
 
@@ -24,6 +26,7 @@ export default {
   name: 'ItemListCard',
 
   props: {
+    id: { type: String, default: '' },
     name: { type: String, default: '(미등록 상품)' },
     desc: { type: String, default: '(미등록 상품 설명란)' },
     discount_price: { type: Number, default: 0 },
@@ -60,42 +63,46 @@ export default {
   margin-right: 10%;
   cursor: pointer;
 
-  .item-profile {
-    width: 160px;
-    height: 160px;
-    margin: 12px 0 8px;
-    background-position: center;
-    background-size: cover;
-  }
+  a {
+    text-decoration: none;
 
-  .item-price {
-    margin-top: 16px;
+    .item-profile {
+      width: 160px;
+      height: 160px;
+      margin: 12px 0 8px;
+      background-position: center;
+      background-size: cover;
+    }
 
-    b {
-      color: red;
-      font-size: 16px;
+    .item-price {
+      margin-top: 16px;
+
+      b {
+        color: red;
+        font-size: 16px;
+        font-weight: bold;
+      }
+
+      span {
+        color: black;
+        font-size: 16px;
+        font-weight: bold;
+      }
+    }
+
+    .item-name {
+      margin: 6px 0;
+      font-size: 18px;
       font-weight: bold;
     }
 
-    span {
-      color: black;
+    .item-desc {
+      width: 180px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
       font-size: 16px;
-      font-weight: bold;
     }
-  }
-
-  .item-name {
-    margin: 6px 0;
-    font-size: 18px;
-    font-weight: bold;
-  }
-
-  .item-desc {
-    width: 180px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    font-size: 16px;
   }
 }
 </style>
