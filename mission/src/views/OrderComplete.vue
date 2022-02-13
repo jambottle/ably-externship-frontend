b898e41626dd6564bee364d92e4e97c3eb40a7af
jambottle <jaewon.walks@gmail.com>
2022-02-13 22:45:41 +0900

    Add: 5주차 미션 요구사항 구현 - (3) 결제 완료 페이지 레이아웃 구현 완료 (OrderComplete.vue)

--- mission/src/views/OrderComplete.vue
+++ mission/src/views/OrderComplete.vue
@@ -1,8 +1,57 @@
-<template>COMPLETE</template>
+<template>
+  <aside id="order-complete">
+    <span>✅<br />주문이 완료되었습니다.</span>
+
+    <router-link to="/">
+      <button
+        class="w3-lightgray w3-large w3-round-large w3-border-0 w3-padding"
+        data-test="home-button"
+      >
+        <strong>🏠 처음으로</strong>
+      </button>
+    </router-link>
+  </aside>
+</template>
 
 <script>
-export default {};
+export default {
+  name: 'OrderCompletePage',
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+#order-complete {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  max-width: 512px;
+  margin: 0 auto 84px;
+  padding: 200px 0;
+
+  span {
+    font-size: 21px;
+    text-align: center;
+  }
+
+  a {
+    width: 100%;
+    max-width: 150px;
+    margin: 15px auto 0;
+    text-decoration: none;
+
+    button {
+      display: block;
+      margin: 8px auto;
+      border: 0;
+      box-shadow: 4px 4px lightgray;
+      cursor: pointer;
+
+      strong {
+        font-weight: 600;
+      }
+    }
+  }
+}
 </style>
