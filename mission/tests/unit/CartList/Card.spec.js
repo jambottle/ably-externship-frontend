5ca055543231630b9d9ce47c40619a681adf3fbd
jambottle <jaewon.walks@gmail.com>
2022-02-14 06:35:13 +0900

    Test: Card 컴포넌트에 대한 단위 테스트 실시 (CartList) - setProps 메서드로 주입한 임의의 데이터를 바탕으로 요구사항을 만족하는 기본적인 렌더링 여부를 확인 - 필요한 라우팅 룰을 불러와 상품의 상세 정보 페이지로 전환시키는 과정을 테스트 내에서 구현하여 검증

---
+++ mission/tests/unit/CartList/Card.spec.js
@@ -0,0 +1,67 @@
+import { mount, flushPromises } from '@vue/test-utils';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from '@/App.vue';
+import CartListCard from '@/components/CartList/Card.vue';
+import ItemListPage from '@/views/ItemList.vue';
+import ItemInfoPage from '@/views/ItemInfo.vue';
+import CartRoutes from '@/router/CartRoutes';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: ItemListPage,
+  },
+  ...CartRoutes,
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+const testItemInfo = {
+  name: '아이언맨 마크 42',
+  desc: '<p>원거리 탈착 기능이 탑재된 고성능 슈트</p>',
+  discount_price: 4000000000,
+  original_price: 5000000000,
+};
+
+describe('Card.vue', () => {
+  it('renders profile image, name, discount rate and price of item', async () => {
+    const wrapper = mount(CartListCard, {
+      global: {
+        plugins: [router],
+      },
+    });
+
+    await wrapper.setProps({
+      ...testItemInfo,
+    });
+
+    expect(wrapper.get('[data-test="item-profile"]').exists()).toBe(true);
+    expect(wrapper.get('[data-test="discount-rate"]').text()).toBe(`${Math.round((1 - (testItemInfo.discount_price / testItemInfo.original_price)) * 100)}%`);
+    expect(wrapper.get('[data-test="discount-price"]').text()).toBe(`${testItemInfo.discount_price.toLocaleString()}원`);
+    expect(wrapper.get('[data-test="item-name"]').text()).toBe(testItemInfo.name);
+    expect(wrapper.get('[data-test="item-desc"]').text()).toBe(testItemInfo.desc);
+  });
+
+  it('routes to ItemInfoPage when the card is clicked', async () => {
+    router.push('/');
+    await router.isReady();
+
+    const container = mount(App, {
+      global: {
+        plugins: [router],
+      },
+    });
+
+    setTimeout(async () => {
+      await container.get('[data-test="item-router"]').trigger('click');
+      await flushPromises();
+
+      expect(container.findComponent(ItemInfoPage).exists()).toBe(true);
+    }, 500);
+  });
+});
