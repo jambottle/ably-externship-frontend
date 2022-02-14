c84fc9735958623decf8cc693b78f2365021244e
jambottle <jaewon.walks@gmail.com>
2022-02-14 09:55:36 +0900

    Add: 5주차 미션 요구사항 구현 - (5) Vuex Store에 장바구니 목록 저장 기능 구현 - 장바구니 API의 활용을 Vuex Store로 옮기고, 장바구니 목록 정보를 state에 저장

--- mission/src/store/index.js
+++ mission/src/store/index.js
@@ -1,15 +1,36 @@
 import { createStore } from 'vuex';
+import Repository from '@/repositories/RepositoryFactory';
+
+const CartRepository = Repository.get('cart');
 
 export default createStore({
   state() {
     return {
       siteName: 'DefJam Streetwear 🥾',
+      cartList: [],
     };
   },
 
-  mutations: {},
+  mutations: {
+    setCartList(state, payload) {
+      state.cartList = payload;
+    },
+  },
 
-  actions: {},
+  actions: {
+    async getCartList(context) {
+      try {
+        const { data, status } = await CartRepository.getCartList();
+        if (status !== 200) {
+          throw new Error('장바구니의 목록을 조회할 수 없습니다.');
+        } else {
+          context.commit('setCartList', data.cart_item);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
 
   modules: {},
 });
