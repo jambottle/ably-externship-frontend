7ee327ddfe088a729dc7b2fab8e437901dbffad4
jambottle <jaewon.walks@gmail.com>
2022-02-17 12:35:35 +0900

    Refactor: 피어 리뷰 반영 - (3) Lazy Loading 적용 - 사이트 접속 시, 처음부터 로딩될 필요가 없다고 판단되는 Order/User/Wish 페이지에 Lazy Loading 적용 등

--- mission/src/router/OrderRoutes.js
+++ mission/src/router/OrderRoutes.js
@@ -1,17 +1,14 @@
-import OrderCheckoutPage from '../views/OrderCheckout.vue';
-import OrderCompletePage from '../views/OrderComplete.vue';
-
 export default [
   {
     path: '/order',
     name: 'OrderCheckoutPage',
-    component: OrderCheckoutPage,
+    component: () => import(/* webpackChunkName: "group-order" */ '@/views/OrderCheckout.vue'),
     meta: { title: '상품 주문 및 결제' },
   },
   {
     path: '/order/complete',
     name: 'OrderCompletePage',
-    component: OrderCompletePage,
+    component: () => import(/* webpackChunkName: "group-order" */ '@/views/OrderComplete.vue'),
     meta: { title: '상품 주문 및 결제 완료' },
   },
 ];
--- mission/src/router/WishRoutes.js
+++ mission/src/router/WishRoutes.js
@@ -1,10 +1,8 @@
-import WishListPage from '../views/WishList.vue';
-
 export default [
   {
     path: '/wish',
     name: 'WishListPage',
-    component: WishListPage,
+    component: () => import(/* webpackChunkName: "group-wish" */ '@/views/WishList.vue'),
     meta: { title: '찜한 상품 목록 조회' },
   },
 ];
